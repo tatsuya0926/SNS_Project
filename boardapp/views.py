885194e3e2316bdc1ad27c41cb677abf6bd5a1f8
‘miyatatsu’ <miyatatsu0926@gmail.com>
2021-10-19 21:36:52 +0900

    Post追加

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,5 +1,11 @@
-from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 def signupfanc(request):
+  object_list = User.objects.all()
+  print(object_list)
+  if request.method == "POST":
+    print()
+  else:
+    print()
   return render(request, 'signup.html', {})
